06c4e8fe19bf5a6a3d8d8bbd381f8c14d9006a2e
kuangbenzhuzhu <kuangbenzhuzhu@gmail.com>
2017-01-18 11:33:15 +0800

    implement part 2

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   resources :jobs
   root 'jobs#index'
 
+  namespace :admin do
+    resources :jobs
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
