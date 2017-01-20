70221bdc1c3066ea888b2d7c0107db2cb26b9bd2
kuangbenzhuzhu <kuangbenzhuzhu@gmail.com>
2017-01-20 21:51:53 +0800

    changes I dont want

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
-puts "这个种子档会自动建立一个admin账号，并且创建10个public jobs,以及10个hidden jobs"
-
-create_account = User.create([email: 'example@gmail.com', password: '123456', password_confirmation: '123456',
-  is_admin: 'true'])
-puts "Admin account created."
-
-create_jobs = for i in 1..10 do
-  Job.create!([title: "Job.no.#{i}", description: "这是种子建立的第#{i}个Public工作", wage_upper_bound: rand(50..99)*100,
-                wage_lower_bound: rand(10..49)*100, is_hidden: "false" ])
-  #sleep rand(1..5)
-end
-
-puts "10 Public jobs created."
-
-create_jobs = for i in 1..10 do
-  Job.create!([title: "Job.no.#{i+10}", description: "这是种子建立的第#{i+10}个hidden工作",
-               wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "true" ])
-  #sleep rand(2..4)
-end
-
-puts "10 Hidden jobs created."
